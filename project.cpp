067399a0bbcf56d973cd2c86fc5f1eebc4e90c95
VirendraOp7 <virendragadhavi96@gmail.com>
2024-08-07 16:38:39 -0400

    Making hash key function

--- project.cpp
+++ project.cpp
@@ -13,5 +13,15 @@ typedef struct Parcel {
 
 typedef struct HashTable {
     Parcel* table[HASH_TABLE_SIZE];
+
 } HashTable;
 
+
+unsigned long djb2_hash(char* str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++) != NULL) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash % HASH_TABLE_SIZE;
+}
